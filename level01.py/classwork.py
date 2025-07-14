030d53e5f91923eabb58f1da4cb460286063669b
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-07-14 13:37:10 +0400

    first commit

---
+++ level01.py/classwork.py
@@ -0,0 +1,14 @@
+#string- aris teqsti romelic moqceulia brwyalebshi
+#integer aris monacemta tipi romelshic shedis mteli ricxvi
+#float aris monacemta tipi romelshic atwiladi ricxvebi shedis
+
+#print aris funqcia, romelsac viyenebt imistvis, rom ekranze rame gamochndes
+print("lile")
+print("dzneladze")
+print(13)
+
+#cvladshi vinaxavt monacemebs rom mere gamoviyenot
+
+saxeli="lile"
+gvari="dzneladze"
+asaki=13
