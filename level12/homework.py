57e2c0aa02d3a63f02fdf1678992fc974bad8b4a
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-08-01 20:11:18 +0400

    tenth commit

---
+++ level12/homework.py
@@ -0,0 +1,12 @@
+# boolean არის მონაცემთა ტიპი რომელსაც აქვს 2 მნიშვნელობა: true და false.
+# binery არის ინფორმაცია გამოხატული მხოლოდ 0-ით და 1-ით. 
+
+print(7 == 7) #true
+print(8 == 9) #false
+print(2 != 3) #true
+print(4 != 4) #false
+print(6 > 1) #true
+print(9 < 2) #false
+
+number1 = print(input(10)) #true
+number2 = print(input(27)) #false
