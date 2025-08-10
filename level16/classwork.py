e536e884b0b701afe492ce7473cbbf27719a71fe
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-08-10 15:40:01 +0400

    fifteenth commit

---
+++ level16/classwork.py
@@ -0,0 +1,22 @@
+#ასაკის შემოტანა
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You are kid")
+else:
+    print("You are adult")
+
+# პაროლკს შემოწმება
+password = input("Enter your password: ")
+
+if password == "1234":
+    print("Password is correct")
+else:
+    print("Password is incorrect")
+
+# დადებითი რიცხვი
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive")
+
---
+++ level16/homework.py
@@ -0,0 +1,37 @@
+password = int(input("Enter your password: "))
+
+if password == "2468":
+      print("password is correct")
+else:
+      print("password is incorrect")
+
+
+age = int(input("Enter your age: "))
+
+if age > 13:
+      print("you are a teenager")
+else:
+      print("you are not a teenager")
+
+
+age = int(input("Enter your age"))
+
+if age < 18:
+      print("you are not an adult")
+else:
+      print("You are an adult")
+
+
+response = input("would you like some food?: (Y/N)")
+if response == "Y":
+      print("have some food")
+else:
+      print("no food for you")
+
+
+age = input("Enter your age")
+if age >= 18:
+      print("you are not signed up")
+else:
+      print("you have to be 18+")
+
