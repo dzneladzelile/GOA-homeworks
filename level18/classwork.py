f864d8d362b82425f7d438364e93cab2d2756ba8
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-08-14 16:11:53 +0400

    seventeenth commit

---
+++ level18/classwork.py
@@ -0,0 +1,19 @@
+x = input()
+if x < 10: 
+    print("არის 10-ზე ნაკლები")
+elif x < 20:
+    print("არის 20-ზე ნაკლები")
+
+
+weather = input()
+
+if weather == "sunny":
+    print("ვივარჯიშებ გარეთ")
+else:
+    print("არ ვივარჯიშებ დღეს")
+
+for i in range(3):
+    name = input("შეიყვანე სახელი: ")
+    print(name)
+
+
---
+++ level18/homework.py
@@ -0,0 +1,13 @@
+# for ციკლი გამოიყენება, როცა გვინდა ერთი და იგივე კოდი რამდენჯერმე გავამეოროთ.მაგალითად:
+
+name = input("Enter your name: ")
+for x in range(10):
+    print(name)
+
+total=0
+for i in range(5):
+    number = int(input("Enter number: "))
+    average = total + number
+average = total // 5
+print("the average is:", average)
+
