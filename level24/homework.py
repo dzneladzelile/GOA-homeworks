5a71ecd3dbf153ca80dbd15d8877611bd4787502
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-08-31 20:39:21 +0400

    twenty-third

---
+++ level24/homework.py
@@ -0,0 +1,13 @@
+# სია პრიგრამირებაში არის ელემენტების დალაგება. ხოლო თითოეულ ელემენტს აქვს თავისი ინდექსი, რომელიც იწყება ნულიდან.
+
+fruits = ["apple","pear","watermelon","banana"]
+fruits[2] = "peach"
+print(fruits)
+
+colors = ["red","blue","green","yellow"]
+colors[3] = "purple"
+print(colors)
+
+numbers = ["10","60","30","40","50"]
+numbers[1] = "20"
+print(numbers)
