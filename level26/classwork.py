733425aa1e368faee663dbb7fa7c615b4b3259c6
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-09-07 18:08:51 +0400

    twenty-sixth commit

---
+++ level26/classwork.py
@@ -0,0 +1,16 @@
+names = ["Ana","Gio","Luka","Nino","Dato"]
+print(names[1:4])  
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers[0:10:2])  
+
+print(numbers[3:7])
+
+print(numbers[2:-2:3])  
+
+print(numbers[::-1])  
+
+print(numbers[9::-1])  
+
+
