56c517b953df5c4ed92d3318b30b43a28ba31838
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-09-10 18:27:25 +0400

    twenty-ninth commit

---
+++ level30/homework.py
@@ -0,0 +1,22 @@
+# ფუნქცია ასრულებს კონკრეტულ ამოცანას და ამარტივებს კოდის განმეორებად გამოყენებას.
+# არგუმენტი არის ინფორმაცია, რომელსაც ფუნქციას ვაძლევთ, რომ იმუშაოს.
+# dot ნოტაცია არის როცა ვწერთ რამეს, მერე წერტილს და შემდეგ ფუნქციას, რომელიც იმ რაღაცაზე მუშაობს.
+
+text = "WORLD"
+print("Hello " + text.lower())
+
+
+name = "geORgi"
+print(name.capitalize())  
+
+
+sentence = "hello my friend"
+print(sentence.title())
+
+
+word = "banana"
+print(word.find('a'))
+
+
+city = "berlin"
+print(city.upper())
