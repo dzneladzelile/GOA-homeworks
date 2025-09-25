4050f8f0072598528f01a561817d27556364f558
dzneladzeliles@gmail.com <dzneladzeliles@gmail.com>
2025-09-25 20:02:29 +0400

    thirty-three commit

---
+++ level34/homework.py
@@ -0,0 +1,15 @@
+def happy_birthday(name, age):
+    print(f"happy birthday to {name}!")
+    print(f"you are {age} years old")
+    print("happy birthday to you!")
+    print()
+
+happy_birthday("Eka", 32)
+
+
+def display_invoice(username, date):
+    print(f"hello {username}")
+    print(f"you created your account on {date}")
+    print()
+
+display_invoice("L.Dzn", "01/12")
