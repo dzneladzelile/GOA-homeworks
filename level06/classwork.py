fddb8e13b16fb4e0033b95338ed0144dad4e8bb8
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:23:55 +0400

    sixth commit

---
+++ level06/classwork.py
@@ -0,0 +1,14 @@
+# input არის ინფორმაცია, რომელსაც მომხმარებელი აწვდის კომპიუტერს (მაგალიტად:კლავიატურა, მიკროფონი, კამერა).
+# output არის ინფორმაცია, რომელსაც კომპიუტერი აწვდის მომხმარებელს (მაგალითად: დინამიკი, პრინტერი)
+
+name = (input("Enter your name"))
+lastname = (input("Enter your lastname"))
+age = int(input("enter your age"))
+
+name = "Lile"
+lastname = "Dzneladze"
+age = "14"
+
+print("my name is " + name)
+print("my lastname is " +  lastname)
+print("i am " + age + "years old")
