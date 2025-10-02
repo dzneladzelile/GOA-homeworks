6d731f7be58271149005db1ee15d9f7a727bdb7d
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:53:22 +0400

    twenty-six commit

---
+++ level26/homework.py
@@ -0,0 +1,10 @@
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(my_list[4])
+print(my_list[6])
+print(my_list[8])
+
+
+reversed_every_second = my_list[::-2]
+print(reversed_every_second)
+
+
