6ed4c879ce07fcf9d447c973d9c18f922c5f8551
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:57:17 +0400

    thirtieth commit

---
+++ level30/classwork.py
@@ -0,0 +1,17 @@
+brand = "bmw"
+print(brand.upper())  
+
+text = "HELLO"
+print(text.lower()) 
+
+city = "LONDON"
+print(city.capitalize())  
+
+sentence = "hello world"
+print(sentence.title())  
+
+word = "apple"
+print(word.find("l"))
+
+
+
