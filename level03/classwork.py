1bc704dc242c696d46335fe5910da7844160f1fe
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:22:12 +0400

    third commit

---
+++ level03/classwork.py
@@ -0,0 +1,19 @@
+number=8
+float_number=3.1
+print(6 + 9)
+print("5"+"6")
+
+print(8 + 7)
+print(9 + 6)
+
+print(12 - 8)
+print(13 - 9)
+
+print(6 * 8)
+print(7 * 3)
+
+print(35 / 7)
+print(40 / 4)
+
+print(63 // 7)
+print(42 // 6)
