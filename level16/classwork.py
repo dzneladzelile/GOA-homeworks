a598c77fad99517115b128f0bc22803a49428103
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:35:43 +0400

    sixteenth commit

---
+++ level16/classwork.py
@@ -0,0 +1,21 @@
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You are kid")
+else:
+    print("You are adult")
+
+
+password = input("Enter your password: ")
+
+if password == "1234":
+    print("Password is correct")
+else:
+    print("Password is incorrect")
+
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive")
+
