b018837e6b27edd185de18931a33b54394c10624
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:29:56 +0400

    twelfth commit

---
+++ level12/classwork.py
@@ -0,0 +1,8 @@
+print(3 == 3) #true
+print(8 != 8) #false
+print(7 < 5) #false
+print(8 > 2) #true
+print(7 >= 8) #false
+print(4 <= 4) #true
+
+print(True and False or 5 > 10 and 100 / 5 > 10 and "Python" == "Python" or 2 * 3 < 5 or 6 + 7 - 9 < 20 and True and 5 > 5.0 or 10 - 10 < 100) #true
