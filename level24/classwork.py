1ea6ab7dd011d5fb848763abb1ca25f19ebbe468
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-02 16:51:45 +0400

    twenty-four commit

---
+++ level24/classwork.py
@@ -0,0 +1,19 @@
+cars = ["BMW", "Toyota", "Mercedes", "Honda", "Ford"]
+print(cars)
+
+print(cars[2])  
+
+print(cars[3])
+
+
+
+names = ["nika", "ana", "giorgi", "mariami", "Tamari", "daviti", "luka"]
+         
+names[0] = "elene"
+names[4] = "saba"
+print(names)
+
+
+names[2], names[6] = names[6], names[2]
+print(names)
+
