92d1c9592f89ae191688fd7bb9095739a4c726b2
dzneladzelile <dzneladzeliles@gmail.com>
2025-10-17 22:23:32 +0400

    forty-five commit

---
+++ level45/classwork.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+if number > 0:
+    print("the number is positive")
+elif number < 0:
+    print("the number is negavite")
+else:
+    print("the number is zero")
