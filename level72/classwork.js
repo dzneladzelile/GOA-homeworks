11d7fc169fdfac3eee51c12f87ea77a353fa0a06
dzneladzelile <dzneladzeliles@gmail.com>
2026-01-17 20:38:39 +0400

    seventy-first commit

---
+++ level72/classwork.js
@@ -0,0 +1,40 @@
+//console არის პანელი, სადაც ჩანს შეტყობინებები და შედეგები.
+// console.log() არის მეთოდი, რომელიც საშუალებას გვაძლევს დაბეჭდოთ ინფორმაცია კონსოლში (მაგალითად რიცხვები, ტექსტები და სხვა მონაცემები).
+
+console.log(42);
+console.log(3.14);
+
+console.log(1234567890123456789n);
+console.log(9876543210987654321n);
+
+console.log('Hello World!');
+console.log("World Hello");
+
+console.log(true);
+console.log(false);
+
+console.log(null);
+
+console.log(undefined);
+
+console.log(Symbol('id'));
+console.log(Symbol('key'));
+
+console.log({name: 'Giga', age: 25});
+console.log({city: 'Tbilisi', country: 'Georgia'});
+
+console.log(5 + 3);  
+console.log(10 + 7); 
+
+console.log(9 - 4); 
+console.log(15 - 6); 
+
+
+console.log(4 * 2);  
+console.log(7 * 3);  
+
+console.log(20 / 4);
+console.log(15 / 3);
+
+console.log(11 % 3);
+console.log(20 % 6);
