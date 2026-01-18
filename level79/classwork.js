a6e921af0a39ecdaaaeb043cbbc8578a453283bc
dzneladzelile <dzneladzeliles@gmail.com>
2026-01-18 23:14:17 +0400

    seventy-fourth commit

---
+++ level79/classwork.js
@@ -0,0 +1,116 @@
+let month = 7;
+if (month === 1) {
+  console.log("january");
+} else if (month === 2) {
+  console.log("february");
+} else if (month === 3) {
+  console.log("march");
+} else if (month === 4) {
+  console.log("april");
+} else if (month === 5) {
+  console.log("may");
+} else if (month === 6) {
+  console.log("june");
+} else if (month === 7) {
+  console.log("july");
+} else if (month === 8) {
+  console.log("August");
+} else if (month === 9) {
+  console.log("september");
+} else if (month === 10) {
+  console.log("october");
+} else if (month === 11) {
+  console.log("november");
+} else if (month === 12) {
+  console.log("decemter");
+} else {
+  console.log("wrong number");
+}
+
+
+let month3 = "july";
+switch (month3) {
+  case "January":
+    console.log(1);
+    break;
+  case "February":
+    console.log(2);
+    break;
+  case "March":
+    console.log(3);
+    break;
+  case "April":
+    console.log(4);
+    break;
+  case "May":
+    console.log(5);
+    break;
+  case "June":
+    console.log(6);
+    break;
+  case "July":
+    console.log(7);
+    break;
+  case "August":
+    console.log(8);
+    break;
+  case "September":
+    console.log(9);
+    break;
+  case "October":
+    console.log(10);
+    break;
+  case "November":
+    console.log(11);
+    break;
+  case "December":
+    console.log(12);
+    break;
+}
+
+
+
+let weekday = "Thursday";
+switch (weekday) {
+  case "Monday":
+    console.log(1);
+    break;
+  case "Tuesday":
+    console.log(2);
+    break;
+  case "Wednesday":
+    console.log(3);
+    break;
+  case "Thursday":
+    console.log(4);
+    break;
+  case "Friday":
+    console.log(5);
+    break;
+  case "Saturday":
+    console.log(6);
+    break;
+  case "Sunday":
+    console.log(7);
+    break;
+}
+
+
+let day = 4;
+if (day === 1) {
+  console.log("monday");
+} else if (day === 2) {
+  console.log("Tuesday");
+} else if (day === 3) {
+  console.log("Wednesday");
+} else if (day === 4) {
+  console.log("Thursday");
+} else if (day === 5) {
+  console.log("Friday");
+} else if (day === 6) {
+  console.log("Saturday");
+} else if (day === 7) {
+  console.log("Sunday");
+} else {
+  console.log("wrong day");
+}
