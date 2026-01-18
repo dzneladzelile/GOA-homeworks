e9fe3956024504543516ef0c88b5745e49f12e4f
dzneladzelile <dzneladzeliles@gmail.com>
2026-01-18 16:45:18 +0400

    seventy-second commit

---
+++ level76/classwork.js
@@ -0,0 +1,26 @@
+console.log(Math.PI);
+let random = Math.floor(Math.random() * 1000) + 1;
+console.log(random);
+
+let name1 = "Luka";
+var age = 22;
+const city = "Tbilisi";
+let sentence = `my name is ${name1}, i am ${age} years and live in ${city}.`;
+console.log(sentence);
+
+
+let num1 = Math.floor(Math.random() * 10) + 1;  
+let num2 = Math.floor(Math.random() * 30) + 1;   
+let result = Math.pow(num1, num2); 
+console.log(`first number: ${num1}, second number: ${num2}`);
+console.log(`result: ${result}`);
+
+
+let numberValue = 10;
+let stringValue = "Hello";
+let booleanValue = true;
+console.log(typeof numberValue);  
+console.log(typeof stringValue);  
+console.log(typeof booleanValue); 
+
+
